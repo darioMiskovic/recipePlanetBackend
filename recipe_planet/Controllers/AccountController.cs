c245ee893955f2b8f015f32b0e8aa024e0cb581f
Dario <orajli98@gmail.com>
2021-08-27 23:04:24 +0200

    account controller&services

---
+++ recipe_planet/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using recipe_planet.Data;
+using recipe_planet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recipe_planet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+       
+
+      
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+
+                
+
+            }
+            catch (Exception)
+            {
+
+                return Problem($"Something Went Wrong in the {nameof(Register)}", statusCode: 500);
+            }
+        }
+
+    }
+}
---
+++ recipe_planet/Services/AccountService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using recipe_planet.Data;
+using recipe_planet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace recipe_planet.Services
+{
+    public class AccountService
+    {
+       
+
+        private readonly UserManager<User> _userManager;
+        private readonly IMapper _mapper;
+
+        public AccountService(UserManager<User> userManager, IMapper mapper)
+        {
+            _userManager = userManager;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> RegisterAccount(UserDTO userDTO, ModelStateDictionary ModelState)
+        {
+            var user = _mapper.Map<User>(userDTO);
+            user.UserName = userDTO.Email;
+            var result = await _userManager.CreateAsync(user, userDTO.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(error.Code, error.Description);
+                }
+                return  new BadRequestObjectResult (ModelState);
+            }
+
+            return Accepted();
+        }
+    }
+}
